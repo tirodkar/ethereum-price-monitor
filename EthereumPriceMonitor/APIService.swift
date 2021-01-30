4309552014b7565ccf00ddfc7c846842b32edc6b
Filip Baumgart <filip.baumgart@rsqtechnologies.com>
2021-01-30 13:26:24 +0100

    add ApiService, models and implement api call + updating view

---
+++ EthereumPriceMonitor/APIService.swift
@@ -0,0 +1,44 @@
+//
+//  APIService.swift
+//  EthereumPriceMonitor
+//
+//  Created by Filip Baumgart on 30/01/2021.
+//
+
+import Foundation
+
+final class APIService {
+  
+  func getEthereumPrice(completionHandler: @escaping (Result<Cryptocurrency, Error>) -> Void) {
+    let url = "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=pln"
+    if let url = URL(string: url) {
+      URLSession.shared.dataTask(with: url) { data, _, error in
+        if let data = data {
+          do {
+            let json: Cryptocurrency = try JSONDecoder().decode(Cryptocurrency.self, from: data)
+            completionHandler(.success(json))
+          } catch let error {
+            print(error.localizedDescription)
+          }
+        }
+      }.resume()
+    }
+  }
+}
+
+struct Cryptocurrency: Decodable {
+  let name: Ethereum
+  
+  enum CodingKeys: String, CodingKey {
+    case name = "ethereum"
+     }
+  
+  struct Ethereum: Decodable {
+    let price: Double
+    
+    enum CodingKeys: String, CodingKey {
+      case price = "pln"
+       }
+  }
+}
+
--- EthereumPriceMonitor/PriceViewController.swift
+++ EthereumPriceMonitor/PriceViewController.swift
@@ -9,13 +9,38 @@ import UIKit
 
 final class PriceViewController: UIViewController {
   
+  @IBOutlet weak private var loadingView: UIView!
   @IBOutlet weak private var currentPriceLabel: UILabel! { didSet {
     currentPriceLabel.text = ""
   }}
   private var price = 0
+  private var apiService = APIService()
+  
+  override func viewDidLoad() {
+    getEthereumPrice()
+  }
   
   @IBAction private func updatePrice(_ sender: Any) {
-    price += 100
-    currentPriceLabel.text = String(price)
+    getEthereumPrice()
+  }
+  
+  private func getEthereumPrice() {
+    loadingView.isHidden = false
+    apiService.getEthereumPrice { [weak self] result in
+      switch result {
+      case .success(let crypto):
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+          self?.updatePriceLabel(with: crypto.name.price)
+          self?.loadingView.isHidden = true
+        }
+      case .failure(let error):
+        print(error.localizedDescription)
+        self?.loadingView.isHidden = true
+      }
+    }
+  }
+  
+  private func updatePriceLabel(with value: Double) {
+    currentPriceLabel.text = "1 ETH = \(String(value)) PLN"
   }
 }
