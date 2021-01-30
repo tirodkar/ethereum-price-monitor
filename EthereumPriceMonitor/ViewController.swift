cd823aee6d4e95d3dd3c871ec58437feeb38dc29
Filip Baumgart <filip.baumgart@rsqtechnologies.com>
2021-01-30 10:45:27 +0100

    Initial Commit

---
+++ EthereumPriceMonitor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EthereumPriceMonitor
+//
+//  Created by Filip Baumgart on 30/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
